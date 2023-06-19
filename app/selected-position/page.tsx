daa4e6ead94e461d8d0897ecbcd8e46ac817ff0d
unknown <muofunanyajennifer1@gmail.com>
2023-06-19 17:45:48 +0100

    feat: Worked on the selected Position route

--- app/selected-position/page.tsx
+++ app/selected-position/page.tsx
@@ -1,197 +1,42 @@
 import React from "react";
+import { SELECTED_CANDIDATE } from "@/utils/config/urls";
 import Image from "next/image";
+import Link from "next/link";
 
 const SelectedPosition = () => {
+  const list = new Array(10).fill(0);
+  //   console.log(list)
   return (
-    <div className="flex flex-col border border-red-700 min-h-screen">
-      <div className="flex justify-center mt-6">
+    <div className="flex flex-col min-h-screen">
+      <div className="flex justify-center m-6">
         <h1 className="text-xl font-semibold text-blue-950">
-          Presidential Candidate
+          Presidential Candidates
         </h1>
       </div>
-      <div className="w-full grid grid-cols-3 place-items-center">
-        <div className="flex flex-col items-center justify-center h-30 w-50 md:h-60 md:w-80 sm:h-40 sm:60 my-5 md:w-80 md:h-80 border shadow-md hover:shadow-lg dark:border-b-white-900 rounded-lg">
-          <span className="relative w-28 h-28">
-            <Image
-              src="/assets/images/avatar.jpg"
-              alt="Avatar"
-              style={{
-                objectFit: "contain",
-                objectPosition: "center",
-              }}
-              fill
-            />
-          </span>
-          <p className="text-lg font-medium">Name</p>
-          <p className="text-lg font-normal">Level</p>
+      <Link href={SELECTED_CANDIDATE}>
+        <div className="w-full grid gap-5 grid-cols-1 px-3 place-items-center cursor-pointer md:grid-cols-3 sm:grid-cols-2">
+          {list.map((data, i) => (
+            <div
+              key={i}
+              className="flex flex-col items-center justify-center h-fit p-5 min-w-40 w-full border shadow-md hover:shadow-lg dark:border-b-white-900 rounded-lg"
+            >
+              <span className="relative w-28 h-28">
+                <Image
+                  src="/assets/images/avatar.jpg"
+                  alt="Avatar"
+                  style={{
+                    objectFit: "contain",
+                    objectPosition: "center",
+                  }}
+                  fill
+                />
+              </span>
+              <p className="text-lg font-medium">Name</p>
+              <p className="text-lg font-normal">Level</p>
+            </div>
+          ))}
         </div>
-        <div className="flex flex-col items-center justify-center h-60 w-80 my-5 md:w-80 md:h-80 border shadow-md hover:shadow-lg dark:border-b-white-900 rounded-lg">
-          <span className="relative w-28 h-28">
-            <Image
-              src="/assets/images/avatar.jpg"
-              alt="Avatar"
-              style={{
-                objectFit: "contain",
-                objectPosition: "center",
-              }}
-              fill
-            />
-          </span>
-          <p className="text-lg font-medium">Name</p>
-          <p className="text-lg font-normal">Level</p>
-        </div>
-        <div className="flex flex-col items-center justify-center h-60 w-80 my-5 md:w-80 md:h-80 border shadow-md hover:shadow-lg dark:border-b-white-900 rounded-lg">
-          <span className="relative w-28 h-28">
-            <Image
-              src="/assets/images/avatar.jpg"
-              alt="Avatar"
-              style={{
-                objectFit: "contain",
-                objectPosition: "center",
-              }}
-              fill
-            />
-          </span>
-          <p className="text-lg font-medium">Name</p>
-          <p className="text-lg font-normal">Level</p>
-        </div>
-        <div className="flex flex-col items-center justify-center h-60 w-80 my-5 md:w-80 md:h-80 border shadow-md hover:shadow-lg dark:border-b-white-900 rounded-lg">
-          <span className="relative w-28 h-28">
-            <Image
-              src="/assets/images/avatar.jpg"
-              alt="Avatar"
-              style={{
-                objectFit: "contain",
-                objectPosition: "center",
-              }}
-              fill
-            />
-          </span>
-          <p className="text-lg font-medium">Name</p>
-          <p className="text-lg font-normal">Level</p>
-        </div>
-        <div className="flex flex-col items-center justify-center h-60 w-80 my-5 md:w-80 md:h-80 border shadow-md hover:shadow-lg dark:border-b-white-900 rounded-lg">
-          <span className="relative w-28 h-28">
-            <Image
-              src="/assets/images/avatar.jpg"
-              alt="Avatar"
-              style={{
-                objectFit: "contain",
-                objectPosition: "center",
-              }}
-              fill
-            />
-          </span>
-          <p className="text-lg font-medium">Name</p>
-          <p className="text-lg font-normal">Level</p>
-        </div>
-        <div className="flex flex-col items-center justify-center h-60 w-80 my-5 md:w-80 md:h-80 border shadow-md hover:shadow-lg dark:border-b-white-900 rounded-lg">
-          <span className="relative w-28 h-28">
-            <Image
-              src="/assets/images/avatar.jpg"
-              alt="Avatar"
-              style={{
-                objectFit: "contain",
-                objectPosition: "center",
-              }}
-              fill
-            />
-          </span>
-          <p className="text-lg font-medium">Name</p>
-          <p className="text-lg font-normal">Level</p>
-        </div>
-        <div className="flex flex-col items-center justify-center h-60 w-80 my-5 md:w-80 md:h-80 border shadow-md hover:shadow-lg dark:border-b-white-900 rounded-lg">
-          <span className="relative w-28 h-28">
-            <Image
-              src="/assets/images/avatar.jpg"
-              alt="Avatar"
-              style={{
-                objectFit: "contain",
-                objectPosition: "center",
-              }}
-              fill
-            />
-          </span>
-          <p className="text-lg font-medium">Name</p>
-          <p className="text-lg font-normal">Level</p>
-        </div>
-        <div className="flex flex-col items-center justify-center h-60 w-80 my-5 md:w-80 md:h-80 border shadow-md hover:shadow-lg dark:border-b-white-900 rounded-lg">
-          <span className="relative w-28 h-28">
-            <Image
-              src="/assets/images/avatar.jpg"
-              alt="Avatar"
-              style={{
-                objectFit: "contain",
-                objectPosition: "center",
-              }}
-              fill
-            />
-          </span>
-          <p className="text-lg font-medium">Name</p>
-          <p className="text-lg font-normal">Level</p>
-        </div>
-        <div className="flex flex-col items-center justify-center h-60 w-80 my-5 md:w-80 md:h-80 border shadow-md hover:shadow-lg dark:border-b-white-900 rounded-lg">
-          <span className="relative w-28 h-28">
-            <Image
-              src="/assets/images/avatar.jpg"
-              alt="Avatar"
-              style={{
-                objectFit: "contain",
-                objectPosition: "center",
-              }}
-              fill
-            />
-          </span>
-          <p className="text-lg font-medium">Name</p>
-          <p className="text-lg font-normal">Level</p>
-        </div>
-        <div className="flex flex-col items-center justify-center h-60 w-80 my-5 md:w-80 md:h-80 border shadow-md hover:shadow-lg dark:border-b-white-900 rounded-lg">
-          <span className="relative w-28 h-28">
-            <Image
-              src="/assets/images/avatar.jpg"
-              alt="Avatar"
-              style={{
-                objectFit: "contain",
-                objectPosition: "center",
-              }}
-              fill
-            />
-          </span>
-          <p className="text-lg font-medium">Name</p>
-          <p className="text-lg font-normal">Level</p>
-        </div>
-        <div className="flex flex-col items-center justify-center h-60 w-80 my-5 md:w-80 md:h-80 border shadow-md hover:shadow-lg dark:border-b-white-900 rounded-lg">
-          <span className="relative w-28 h-28">
-            <Image
-              src="/assets/images/avatar.jpg"
-              alt="Avatar"
-              style={{
-                objectFit: "contain",
-                objectPosition: "center",
-              }}
-              fill
-            />
-          </span>
-          <p className="text-lg font-medium">Name</p>
-          <p className="text-lg font-normal">Level</p>
-        </div>
-        <div className="flex flex-col items-center justify-center h-60 w-80 my-5 md:w-80 md:h-80 border shadow-md hover:shadow-lg dark:border-b-white-900 rounded-lg">
-          <span className="relative w-28 h-28">
-            <Image
-              src="/assets/images/avatar.jpg"
-              alt="Avatar"
-              style={{
-                objectFit: "contain",
-                objectPosition: "center",
-              }}
-              fill
-            />
-          </span>
-          <p className="text-lg font-medium">Name</p>
-          <p className="text-lg font-normal">Level</p>
-        </div>
-       
-      </div>
+      </Link>
     </div>
   );
 };
--- utils/config/urls.ts
+++ utils/config/urls.ts
@@ -3,3 +3,4 @@ export const ELECTIONS = "/elections";
 export const STATISTICS_ROUTE = "/statistics";
 export const ELECTION_POSITION = "/election-position";
 export const SELECTED_POSITION = "/selected-position";
+export const SELECTED_CANDIDATE = "/selected-candidate";
